b54363a452029c591d52b1c830081e9313561845
Thabang Selowa <mohale20cobra@gmail.com>
2024-07-26 09:49:16 +0200

    Add guardians preferences

--- index.js
+++ index.js
@@ -11,24 +11,55 @@ const songs = [
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    { title: "I don't want to go around", artist: "Bongi Dube", genre: "pop"},
+    { title: "The Chain", artist: "Fleetwood Mac", genre: "pop"},
+    { title: "Salakanjaloke", artist: "Babes Wodumo", genre:  "Qom"},
+    { title: "Nkalakata", artist: "Mandoza", genre:  "Kwaito"},
+   // { title: "", artist: "", genre:  ""}
     // Feel free to add even more songs
 ];
 
 
 // Object containing each Guardian's preferred genre
 const guardians = {
-    "Star-Lord": "Rock",
-    "Gamora": "Pop",
+    Benjamin: "Rock",
+    Puso: "Pop",
+    Mmathabo: "R&B",
+    Annastasia: "Rock",
+    Thabang: "Pop",
+    
     // Add preferences for Drax, Rocket, and Groot
+    
+
 };
+
+guar.forEach(generatePlaylist => {
+    songs[index] = guar.property("");
+    
+});
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
     // Use the map() function to create playlists for each Guardian
     // Your code here
+    
+    //const BenjaminPlaylist = guardians.map(playlist => playlist.genre);
+   // const AnnastasiaPlaylist = songs.map(playlist => playlist.genre);
+   // const MmathaboPlaylist = songs.map(playlist => playlist.genre);
+  //  const PusoPlaylist = songs.map(playlist => playlist.genre);
+//const ThabangPlaylist = songs.map(playlist => playlist.genre, );
+    //const  = catwomanGadgets.map(gadget => gadget.name);
+    
+   // console.log("Benjamin's PlayList:", BenjaminPlaylist);
+   // console.log("Annastasia's Playlist:", AnnastasiaPlaylist);
+   // console.log("Mmathabo's Playlist:", MmathaboPlaylist);
+   // console.log("Puso's Playlist:", PusoPlaylist);
+   // console.log("Thabang's Playlist:", ThabangPlaylist );
+    //console.log("CatWoman's Gadgets:", catwomanGadgetNames);
+  //  playlist.forEach((guardians) => console.log(guardians));
+    
+
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
 generatePlaylist(guardians, songs);
-
-
